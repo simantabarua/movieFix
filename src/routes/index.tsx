d8f941367e6daed9f4698a7405c2ff7f243d5a81
simanta <simanta.barua@goama.com>
2025-08-22 01:35:35 +0600

    loader

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,33 +1,79 @@
 import { createBrowserRouter } from "react-router";
-import SearchPage from "../pages/SearchPage";
-import MovieDetails from "../pages/MovieDetails";
-import LoginPage from "../pages/LoginPage";
+import { lazy, Suspense } from "react";
 import MainLayout from "../layout/MainLayout";
 import PrivateRoute from "../components/PrivateRoute";
-import Movies from "../pages/Movies";
-import WatchListPage from "../pages/WatchListPage";
-import NotFound from "../pages/404";
+import Loader from "../components/Loader";
+
+const SearchPage = lazy(() => import("../pages/SearchPage"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const Movies = lazy(() => import("../pages/Movies"));
+const WatchListPage = lazy(() => import("../pages/WatchListPage"));
+const NotFound = lazy(() => import("../pages/404"));
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: MainLayout,
     children: [
-      { index: true, Component: SearchPage },
-      { path: "search", Component: SearchPage },
-      { path: "movie/:id", Component: MovieDetails },
-      { path: "movies", Component: Movies },
-
+      {
+        index: true,
+        Component: () => (
+          <Suspense fallback={<Loader />}>
+            <SearchPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "search",
+        Component: () => (
+          <Suspense fallback={<Loader />}>
+            <SearchPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "movie/:id",
+        Component: () => (
+          <Suspense fallback={<Loader />}>
+            <MovieDetails />
+          </Suspense>
+        ),
+      },
+      {
+        path: "movies",
+        Component: () => (
+          <Suspense fallback={<Loader />}>
+            <Movies />
+          </Suspense>
+        ),
+      },
       {
         path: "watchlist",
         Component: () => (
           <PrivateRoute>
-            <WatchListPage />
+            <Suspense fallback={<Loader />}>
+              <WatchListPage />
+            </Suspense>
           </PrivateRoute>
         ),
       },
-      { path: "login", Component: LoginPage },
-      { path: "*", Component: NotFound },
+      {
+        path: "login",
+        Component: () => (
+          <Suspense fallback={<Loader />}>
+            <LoginPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "*",
+        Component: () => (
+          <Suspense fallback={<Loader />}>
+            <NotFound />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
